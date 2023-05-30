e039d00ca62166d1e3b9b57df6f7f9a4907f4699
Thomas Dupont <thomas.dupont@ugent.be>
2023-05-30 14:34:06 +0200

    fix: remove comment after config.php line
    feat: mail tls

--- config.php
+++ config.php
@@ -47,7 +47,8 @@ class Config {
     // GOOGLE CALENDAR SYNC
     // ------------------------------------------------------------------------
 
-    const GOOGLE_SYNC_FEATURE   = FALSE; // Enter TRUE or FALSE
+    // Enter TRUE or FALSE
+    const GOOGLE_SYNC_FEATURE   = FALSE;
     const GOOGLE_PRODUCT_NAME   = '';
     const GOOGLE_CLIENT_ID      = '';
     const GOOGLE_CLIENT_SECRET  = '';
